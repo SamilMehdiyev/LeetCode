ef79f672a711c02e480f753eedd49c6de3baae14
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2019-09-24 17:28:20 +0400

    first commit

---
+++ LeetCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question2 q2 = new Question2();
+            ListNode result = q2.AddTwoNumbers(BigInteger.Parse("1000000000000000000000000000001"), BigInteger.Parse("564"));
+            Console.WriteLine(q2.getValue(result));
+        }
+    }
+}
---
+++ LeetCode/Question2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    class Question2
+    {
+        public ListNode AddTwoNumbers(BigInteger num1, BigInteger num2)
+        {
+            return AddTwoNumbers(generateListNode(num1), generateListNode(num2));
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            string strOne = getValueFromListNode(l1);
+            string strTwo = getValueFromListNode(l2);
+
+            var numOne = parseValueToNumber(strOne);
+            var numTwo = parseValueToNumber(strTwo);
+
+            return generateListNode(numOne + numTwo);
+        }
+
+        public string getValue(ListNode node)
+        {
+            return string.Join("", getValueFromListNode(node));
+        }
+
+        private string getValueFromListNode(ListNode listNode)
+        {
+            string value = listNode.val.ToString();
+            if (listNode.next != null)
+            {
+                value += getValueFromListNode(listNode.next);
+            }
+
+            return value;
+        }
+
+        private BigInteger parseValueToNumber(string str)
+        {
+            return BigInteger.Parse(string.Join("", str.Reverse()));
+        }
+
+        private ListNode generateListNode(BigInteger number)
+        {
+            ListNode listNode = null;
+
+            foreach (var item in number.ToString())
+            {
+                if(listNode == null)
+                {
+                    listNode = new ListNode((int)char.GetNumericValue(item));
+                    continue;
+                }
+
+                ListNode node = new ListNode((int)char.GetNumericValue(item));
+                node.next = listNode;
+
+                listNode = node;
+            }
+
+            return listNode;
+        }
+    }
+}
