676cba08a1a5c47da09b598fa06fe14bb1188bd4
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2019-09-30 14:04:35 +0400

    Added Problem1 - Two Sum

---
+++ LeetCode/Problems/Easy/Problem1 - Two Sum/Problem1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace LeetCode.Problems.Easy
+{
+    class Problem1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                        return new int[] { i, j };
+                }
+            }
+            return new int[] { };
+        }
+    }
+}
