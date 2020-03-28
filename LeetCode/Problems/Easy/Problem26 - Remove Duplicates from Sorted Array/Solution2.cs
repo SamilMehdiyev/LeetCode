babe0193868a8fa2c471cb1e87a0bb366bde05d4
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-03-28 18:35:36 +0400

    Added Solution with O(n) time complexity for the Problem 26 - Remove Duplicates from Sorted Array

---
+++ LeetCode/Problems/Easy/Problem26 - Remove Duplicates from Sorted Array/Solution2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Easy.Problem26
+{
+    class Solution2
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null)
+                return 0;
+
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+
+            int i = 1;
+
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[j - 1])
+                    nums[i++] = nums[j];
+            }
+
+            return i;
+        }
+    }
+}
