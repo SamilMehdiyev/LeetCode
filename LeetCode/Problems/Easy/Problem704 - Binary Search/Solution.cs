98eb1a09a97dc16378f8ad408c928dd5714f1983
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-03-29 23:56:01 +0400

    Added Solution for the Problem 704 - Binary Search

--- LeetCode/Problems/Easy/Problem704 - Binary Search/Solution.cs
+++ LeetCode/Problems/Easy/Problem704 - Binary Search/Solution.cs
@@ -8,5 +8,25 @@ namespace LeetCode.Problems.Easy.Problem704
 {
     class Solution
     {
+        public int Search(int[] nums, int target)
+        {
+            var low = 0;
+            var high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+
+                if (nums[mid] < target)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
     }
 }
