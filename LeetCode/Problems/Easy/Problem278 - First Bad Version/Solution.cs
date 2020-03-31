0b82d64d07c33e2586a97e4ec0cf104f4e44166e
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-03-31 13:06:11 +0400

    Added Problem 278 - First Bad Version

---
+++ LeetCode/Problems/Easy/Problem278 - First Bad Version/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Easy.Problem278___First_Bad_Version
+{
+    class Solution
+    {
+    }
+}
