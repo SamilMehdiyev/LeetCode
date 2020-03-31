8ca6b038c4c8c88bf6da512938ff8c672505ef22
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-03-31 17:06:32 +0400

    Added Solution for the Problem 162 0 Find Peak Element

--- LeetCode/Problems/Medium/Problem162 - Find Peak Element/Solution.cs
+++ LeetCode/Problems/Medium/Problem162 - Find Peak Element/Solution.cs
@@ -2,5 +2,28 @@
 {
     class Solution
     {
+        public int FindPeakElement(int[] nums)
+        {
+            var start = 0;
+            var end = nums.Length;
+
+            while (start < end)
+            {
+                var mid = (start + end) / 2;
+
+                var next = (mid + 1) % nums.Length;
+                var prev = (mid + nums.Length - 1) % nums.Length;
+
+                if (nums[prev] <= nums[mid] && nums[mid] >= nums[next])
+                    return mid;
+
+                if (nums[prev] <= nums[mid] && nums[mid] <= nums[next])
+                    start = mid + 1;
+                else
+                    end = mid;
+            }
+
+            return -1;
+        }
     }
 }
