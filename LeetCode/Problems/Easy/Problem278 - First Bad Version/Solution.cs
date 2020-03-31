8083e6bd71b1bbc8205c2d3ad9da59f5a29f3aca
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-03-31 13:15:52 +0400

    Added Solution for the Problem 278 0 First Bad Version

--- LeetCode/Problems/Easy/Problem278 - First Bad Version/Solution.cs
+++ LeetCode/Problems/Easy/Problem278 - First Bad Version/Solution.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode.Problems.Easy.Problem278___First_Bad_Version
+﻿namespace LeetCode.Problems.Easy.Problem278
 {
     class Solution
     {
+        public int FirstBadVersion(int n)
+        {
+            long start = 1;
+            long end = n;
+
+            while (start < end)
+            {
+                long mid = (start + end) / 2;
+
+                var isBadVersion = IsBadVersion((int)mid);
+
+                if (isBadVersion)
+                    end = mid;
+                else
+                    start = mid + 1;
+            }
+
+            if (start != n && IsBadVersion((int)start)) return (int)start;
+            return (int)end;
+        }
+
+        private bool IsBadVersion(int num)
+        {
+            var guess = 4;
+
+            if (num < guess)
+                return false;
+            else if (num > guess)
+                return true;
+            else
+                return true;
+        }
     }
 }
