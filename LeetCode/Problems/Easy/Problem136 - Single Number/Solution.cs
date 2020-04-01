891be8b5a22abce5a7d3fe7ae6e8005ece7080eb
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-01 20:19:21 +0400

    Added Problem 136 - Single Number

---
+++ LeetCode/Problems/Easy/Problem136 - Single Number/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Easy.Problem136___Single_Number
+{
+    class Solution
+    {
+    }
+}
