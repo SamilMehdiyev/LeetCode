7585dc043c80d0a5170c20e7a20311f7a6cd6990
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-02 21:45:10 +0400

    Added Problem 658 - Find K Closest Elements

---
+++ LeetCode/Problems/Medium/Problem658 - Find K Closest Elements/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium.Problem658
+{
+    class Solution
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            return null;
+        }
+    }
+}
