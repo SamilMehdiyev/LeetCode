7e645163a71a32bbe95bb39f62b50090c96b8cd3
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-05 15:03:58 +0400

    Added Solution for the Problem 121 - Best Time to Buy and Sell Stock

--- LeetCode/Problems/Easy/Problem121 - Best Time to Buy and Sell Stock/Solution.cs
+++ LeetCode/Problems/Easy/Problem121 - Best Time to Buy and Sell Stock/Solution.cs
@@ -2,5 +2,41 @@
 {
     class Solution
     {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length < 2)
+                return 0;
+
+            var buyAt = 0;
+            var sellAt = 0;
+            var maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                var prev = prices[i - 1];
+                var current = prices[i];
+
+                if (prev > current)
+                {
+                    var profit = prices[sellAt] - prices[buyAt];
+
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                        sellAt = 0;
+                    }
+
+                    if (prices[buyAt] > prices[i])
+                        buyAt = i;
+                }
+
+                if (sellAt < buyAt)
+                    sellAt = buyAt;
+
+                sellAt = i;
+            }
+            
+            return (prices[sellAt] - prices[buyAt]) > maxProfit ? (prices[sellAt] - prices[buyAt]) : maxProfit;
+        }
     }
 }
