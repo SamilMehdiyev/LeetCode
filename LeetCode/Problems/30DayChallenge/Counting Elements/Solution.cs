d40350388db5e9967a107365160aa710f0917b83
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-07 21:28:22 +0400

    Added Problem - Counting Elements

---
+++ LeetCode/Problems/30DayChallenge/Counting Elements/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems._30DayChallenge.Counting_Elements
+{
+    class Solution
+    {
+        public int CountElements(int[] arr)
+        {
+            return 0;
+        }
+    }
+}
