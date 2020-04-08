38c3dbb6718b866d520c0d36256b9fe99365620f
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-09 01:48:07 +0400

    Added Problem 876 - Middle of the Linked List

---
+++ LeetCode/Problems/Easy/Problem876 - Middle of the Linked List/Solution.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Portfolio;
+
+namespace LeetCode.Problems.Easy.Problem876
+{
+    class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            return null;
+        }
+    }
+}
