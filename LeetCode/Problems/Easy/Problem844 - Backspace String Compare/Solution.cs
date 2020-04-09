491d532e351b788e7e2941ddf62869bb61298d02
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-09 16:46:56 +0400

    Added Solution for the Problem 844  - Backspace String Compare

--- LeetCode/Problems/Easy/Problem844 - Backspace String Compare/Solution.cs
+++ LeetCode/Problems/Easy/Problem844 - Backspace String Compare/Solution.cs
@@ -4,7 +4,35 @@
     {
         public bool BackspaceCompare(string S, string T)
         {
-            return false;
+            S = this.processString(S);
+            T = this.processString(T);
+            return S.Equals(T);
+        }
+
+        private string processString(string str)
+        {
+            var result = "";
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i].Equals('#'))
+                {
+                    if (!result.Equals(""))
+                        result = result.Substring(0, result.Length - 1);
+
+                    continue;
+                }
+
+                if (i + 1 < str.Length && str[i + 1].Equals('#'))
+                {
+                    i += 1;
+                    continue;
+                }
+
+                result += str[i];
+            }
+
+            return result;
         }
     }
 }
