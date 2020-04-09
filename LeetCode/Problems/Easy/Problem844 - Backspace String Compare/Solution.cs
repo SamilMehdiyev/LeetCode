ff499e275ff2dcba1327bd61074b21fb854af3ad
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-09 16:37:49 +0400

    Added Problem 844 - Backspace String Compare

---
+++ LeetCode/Problems/Easy/Problem844 - Backspace String Compare/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Easy.Problem844
+{
+    class Solution
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            return false;
+        }
+    }
+}
