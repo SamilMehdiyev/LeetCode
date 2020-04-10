399aabbf06220f4f1d619789fbda99b95e9cb3fd
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-11 01:30:19 +0400

    Added Solution for the Problem 155 - Min Stack

--- LeetCode/Problems/Easy/Problem155 - Min Stack/MinStack.cs
+++ LeetCode/Problems/Easy/Problem155 - Min Stack/MinStack.cs
@@ -1,32 +1,62 @@
-﻿namespace LeetCode.Problems.Easy.Problem155
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy.Problem155
 {
     public class MinStack
     {
 
+        private IList<int> _stack;
+        private IList<int> _minValueStack;
+
+        private bool isEmpty;
         /** initialize your data structure here. */
+
         public MinStack()
         {
+            _stack = new List<int>();
+            _minValueStack = new List<int>();
 
+            isEmpty = true;
         }
 
         public void Push(int x)
         {
+            if (isEmpty)
+                _minValueStack.Add(x);
 
+            if (x <= _minValueStack[_minValueStack.Count - 1])
+                _minValueStack.Add(x);
+
+            _stack.Add(x);
+            isEmpty = false;
         }
 
         public void Pop()
         {
+            if (_minValueStack[_minValueStack.Count - 1].Equals(_stack[_stack.Count - 1]))
+                _minValueStack.RemoveAt(_minValueStack.Count - 1);
 
+            if (_stack.Count > 0)
+                _stack.RemoveAt(_stack.Count - 1);
+
+            if (_stack.Count == 0)
+                isEmpty = true;
         }
 
         public int Top()
         {
-            return 0;
+            if (isEmpty)
+                return 0;
+
+            return _stack[_stack.Count - 1];
         }
 
         public int GetMin()
         {
-            return 0;
+            if (isEmpty)
+                return 0;
+
+            return _minValueStack[_minValueStack.Count - 1];
         }
     }
 }
