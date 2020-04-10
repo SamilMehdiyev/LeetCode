128fd9493fca42085813510a00fa36fa1fc9f678
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-11 01:09:46 +0400

    Added MinStack Class to Problem 155 - Min Stack

---
+++ LeetCode/Problems/Easy/Problem155 - Min Stack/MinStack.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems.Easy.Problem155
+{
+    public class MinStack
+    {
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        public int Top()
+        {
+            return 0;
+        }
+
+        public int GetMin()
+        {
+            return 0;
+        }
+    }
+}
