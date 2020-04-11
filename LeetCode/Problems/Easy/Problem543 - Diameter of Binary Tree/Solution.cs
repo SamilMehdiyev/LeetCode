78f14983a62fb514c6fafae9d9f1320ec02b3598
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2020-04-12 02:42:23 +0400

    Added Solution for the Problem 543 - Diameter of Binary Tree

--- LeetCode/Problems/Easy/Problem543 - Diameter of Binary Tree/Solution.cs
+++ LeetCode/Problems/Easy/Problem543 - Diameter of Binary Tree/Solution.cs
@@ -1,6 +1,27 @@
-﻿namespace LeetCode.Problems.Easy.Problem543
+﻿using LeetCode.Portfolio;
+
+namespace LeetCode.Problems.Easy.Problem543
 {
-    class Solution
+    public class Solution
     {
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            int[] longestPath = new int[1];
+            processTree(root, longestPath);
+
+            return longestPath[0];
+        }
+
+        private int processTree(TreeNode node, int[] longestPath)
+        {
+            if (node == null)
+                return 0;
+
+            var leftResult = processTree(node.left, longestPath);
+            var rightResult = processTree(node.right, longestPath);
+
+            longestPath[0] = Math.Max(longestPath[0], leftResult + rightResult);
+            return Math.Max(leftResult, rightResult) + 1;
+        }
     }
 }
